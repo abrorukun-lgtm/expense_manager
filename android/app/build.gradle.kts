0cae9bfd9eae68a24e1b0bbe8181248adbb5646b
abrorukun-lgtm <abrorukun-lgtm@gmail.com>
2026-06-25 13:32:48 +0500

    Use Flutter signing with key.properties

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,15 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keyProperties = Properties()
+val keyPropertiesFile = rootProject.file("key.properties")
+if (keyPropertiesFile.exists()) {
+    keyProperties.load(FileInputStream(keyPropertiesFile))
 }
 
 android {
@@ -21,9 +30,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keyProperties["keyAlias"] as String
+            keyPassword = keyProperties["keyPassword"] as String
+            storeFile = keyProperties["storeFile"]?.let { file(it) }
+            storePassword = keyProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
-            signingConfig = null
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
